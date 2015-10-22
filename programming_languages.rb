730d0307c47f321c7f26e74daecb5b985ac436dd
halfields <hfields697@aol.com>
2015-10-22 15:39:20 +0000

    done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  # Add a :style key at lower level in hash whose value consists of empty array
+  languages.each do |style, language|
+    language.each do |name, attributes|
+      attributes[:style] = []
+    end
+  end
+  languages.each do |style, language|
+    language.each do |name, attributes|
+#  Is language already included? If so, combine style info
+      if new_hash.has_key?(name)
+        new_hash[name][:style] << style
+      else
+        attributes[:style] << style
+        new_hash[name] = attributes
+      end
+    end
+  end
+  new_hash
 end
